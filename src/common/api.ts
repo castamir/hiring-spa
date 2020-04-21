b935aea007de527fd95e4834b0d81b9d640b41aa
Mira Paulik <miras.paulik.primehammer@gmail.com>
2020-04-21 06:43:39 +0200

    - chore: cleanup

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,32 +1,30 @@
 import { CandidateReadDto, State } from "../domain/candidate";
 
 export const getCandidates = () => {
-  const x: CandidateReadDto[] = [
-    {
-      id: "ID 1",
-      email: "jedna@domain.com",
-      name: "John",
-      surname: "Doe",
-      state: State.NEW_CONTACT,
-      nationality: "Czech",
-      links: [],
-      location: "Brno",
-    },
-    {
-      id: "ID 2",
-      email: "dva@domain.com",
-      name: "Eva",
-      surname: "Dlouha",
-      state: State.APPROVED,
-      nationality: "Slovak",
-      links: [],
-      location: "Kosice",
-    },
-  ];
-
   return new Promise<CandidateReadDto[]>((resolve) => {
     setTimeout(() => {
-      resolve(x);
+      resolve([
+        {
+          id: "ID 1",
+          email: "jedna@domain.com",
+          name: "John",
+          surname: "Doe",
+          state: State.NEW_CONTACT,
+          nationality: "Czech",
+          links: [],
+          location: "Brno",
+        },
+        {
+          id: "ID 2",
+          email: "dva@domain.com",
+          name: "Eva",
+          surname: "Dlouha",
+          state: State.APPROVED,
+          nationality: "Slovak",
+          links: [],
+          location: "Kosice",
+        },
+      ]);
     }, 5000);
   });
 };
